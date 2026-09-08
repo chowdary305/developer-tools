a12656baa17db803d44a5eb1966c42347099d572
chowdary305 <chinnaripamidi@gmail.com>
2026-09-08 11:54:54 +0530

    Add files via upload

--- python2.py
+++ python2.py
@@ -1,7 +1,12 @@
-l=int(input("enter length"))
-b=int(input("enter breadth"))
-area=l*b
-perimeter=2*(l+b)
-print("area is;",area)
-print("perimeter is;",perimeter)
-
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> p=float(input("enter principal"))
+enter principal6
+>>> r=float(input("enter rate"))
+enter rate8
+>>> t=float(input("enter time"))
+enter time4
+>>> amount=(1+r/100)-p
+>>> ci=(p*amount)-p
+>>> print("compound intrest=",ci)
+compound intrest= -35.519999999999996
