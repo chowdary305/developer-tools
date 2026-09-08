97c37bace83685fe7b4dfe89e38b8f12cbd8e51f
chowdary305 <chinnaripamidi@gmail.com>
2026-09-08 11:41:57 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,16 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=int(input("enter your number"))
+enter your number2
+>>> b=int(input("enter your number"))
+enter your number4
+>>> c=int(input("enter your number"))
+enter your number6
+>>> sum=a+b+c
+>>> avg=sum
+>>> print(avg)
+12
+>>> 
+
+... 
+>>> KeyboardInterrupt
